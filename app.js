639cc457502fe62c18b93f20f94ef2ae6cdd0252
olabdulhaq <olabdulhaq@gmail.com>
2023-09-07 03:37:35 +0100

    :adhesive_bandage: fix cors

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const fs = require('fs');
+const cors = require('cors');
 const app = express();
 const port = 8080;
+
+app.use(cors());
 
 // Middleware to parse JSON requests
 app.use(express.json());
